92b621adc868f7364c8ca7a668279b84732c3ee3
ramsha17ishaq <ramshaishaq02@gmail.com>
2022-07-05 16:57:58 +0530

    turtle file

---
+++ t12.py
@@ -0,0 +1,24 @@
+from turtle import *
+import random
+
+speed('fastest')
+s = Screen()
+s.setup(500,500)
+colors = ['pink','orange']
+for i in range(50):
+    x = random.randint(-200,200)
+    y = random.randint(-200,200)
+    penup()
+    goto(x,y)
+    pendown()
+    pensize(random.randint(1,2))
+    pencolor(colors[random.randint(0,1)])
+    radius = random.randint(5,30)
+    fillcolor('red')
+    begin_fill()
+    for i in range(6):
+        circle(radius)
+        lt(60)
+        end_fill()
+
+mainloop()
---
+++ t9.py
@@ -0,0 +1,13 @@
+from turtle import *
+
+speed('normal')
+
+side = 5
+shapesize(2,2)
+for i in range(side):
+    dot(25,'black')
+    forward(100)
+    left(360/side)
+    dot(50,'red')
+
+mainloop()
